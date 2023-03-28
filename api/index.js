1093d59005af6f4a362c76f06b67ce1b65260fa8
Saiful <msisaifulsaif@gmail.com>
2023-03-28 20:01:26 +0600

    add logging system using morgan and winston

--- api/index.js
+++ api/index.js
@@ -1,5 +1,32 @@
+const httpStatus = require("http-status");
 const express = require("express");
-const app = express();
+const cors = require("cors");
+const morgan = require("./config/morgan");
+const logger = require("./config/logger");
+const config = require("./config/config");
+const ApiError = require("./utils/ApiError");
+const { errorConverter, errorHandler } = require("./middlewares/error");
+
 const port = process.env.PORT || 5555;
+const app = express();
+// enable cors
+app.use(cors());
+app.options("*", cors());
+
+if (config.env !== "test") {
+  app.use(morgan.successHandler);
+  app.use(morgan.errorHandler);
+}
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(res.json({ error: new ApiError(httpStatus.NOT_FOUND, "Not found") }));
+});
+
+// convert error to ApiError, if needed
+app.use(errorConverter);
+
+// handle error
+app.use(errorHandler);
 
 app.listen(port, () => console.log("listining on port ", port));
