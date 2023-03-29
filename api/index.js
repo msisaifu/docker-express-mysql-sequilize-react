2d970e1171661d47cffc7a5a4796201c84158e01
Saiful <msisaifulsaif@gmail.com>
2023-03-29 09:42:41 +0600

    [api] folder restructure

--- api/index.js
+++ api/index.js
@@ -1,11 +1,10 @@
 const httpStatus = require("http-status");
 const express = require("express");
 const cors = require("cors");
-const morgan = require("./config/morgan");
-const logger = require("./config/logger");
-const config = require("./config/config");
-const ApiError = require("./utils/ApiError");
-const { errorConverter, errorHandler } = require("./middlewares/error");
+const morgan = require("./src/config/morgan");
+const config = require("./src/config/config");
+const ApiError = require("./src/utils/ApiError");
+const { errorHandler } = require("./src/middlewares/error");
 
 const port = process.env.PORT || 5555;
 const app = express();
@@ -20,11 +19,8 @@ if (config.env !== "test") {
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
-  next(res.json({ error: new ApiError(httpStatus.NOT_FOUND, "Not found") }));
+  res.send({ error: new ApiError(httpStatus.NOT_FOUND, "Not found") });
 });
-
-// convert error to ApiError, if needed
-app.use(errorConverter);
 
 // handle error
 app.use(errorHandler);
