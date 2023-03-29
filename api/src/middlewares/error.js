e7325edec1b68d96cc76c21344f84b1b5be8f0a8
Saiful <msisaifulsaif@gmail.com>
2023-03-29 11:23:20 +0600

    [api fix] database error handling

--- api/src/middlewares/error.js
+++ api/src/middlewares/error.js
@@ -1,15 +1,16 @@
 const httpStatus = require("http-status");
 const config = require("../config/config");
 const logger = require("../config/logger");
+const { Sequelize } = require("sequelize");
 const ApiError = require("../utils/ApiError");
 
 const errorConverter = (err, req, res, next) => {
   let error = err;
   if (!(error instanceof ApiError)) {
     const statusCode =
-      error.statusCode ||
-      httpStatus.BAD_REQUEST ||
-      httpStatus.INTERNAL_SERVER_ERROR;
+      error.statusCode || error instanceof Sequelize.DatabaseError
+        ? httpStatus.BAD_REQUEST
+        : httpStatus.INTERNAL_SERVER_ERROR;
     const message = error.message || httpStatus[statusCode];
     error = new ApiError(statusCode, message, false, err.stack);
   }
